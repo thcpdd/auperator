caba81c16e4037c031029be580d91ce9a32ee3b8
Rainbow <1834763300@qq.com>
2026-03-16 21:42:57 +0800

    refactor(collector): 移除已废弃的日志源抽象基类
    - 删除了日志源抽象基类 BaseLogSource 及其实现文件
    - 清理了相关导入和 __all__ 中的 BaseLogSource
    - 简化日志采集器模块结构，减少冗余代码
    - 移除无用的日志源模块文件和注释内容

--- src/auperator/collector/__init__.py
+++ src/auperator/collector/__init__.py
@@ -1,9 +1,6 @@
-"""日志采集器模块"""
-
 from .adapters import BaseLogAdapter, VectorAdapter
 from .handlers import BaseLogHandler, ConsoleHandler
 from .models import LogEntry, LogLevel, LogSource
-from .sources.base import BaseLogSource
 from .vector_consumer import VectorRedisConsumer
 
 __all__ = [
@@ -12,7 +9,6 @@ __all__ = [
     "LogLevel",
     "LogSource",
     # 抽象基类
-    "BaseLogSource",
     "BaseLogAdapter",
     "BaseLogHandler",
     # Vector 集成
--- src/auperator/collector/sources/__init__.py
+++
@@ -1,5 +0,0 @@
-"""日志源模块"""
-
-from .base import BaseLogSource
-
-__all__ = ["BaseLogSource"]
